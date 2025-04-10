05bfb2b1688116b2445ac1eccd256de66f07e297
Gregory <grbolivar@outlook.com>
2025-04-10 14:20:19 -0400

    #231: Los botones “Ver cómo funciona”, “Imprimir cuestionario de contexto” e “imprimir cuestionario” hacerlos un poco más pequeños y poner en una sola linea debajo del historial de cuestionarios y retroalimentación

--- src/pages/resources/components/SurveysList.js
+++ src/pages/resources/components/SurveysList.js
@@ -6,11 +6,11 @@ import styles from "../Resources.styl";
 
 import { isDirector, isDirectorOrTeacher } from "~/helpers/users";
 import {
-  setSelectedSurvey,
-  surveyAnswered,
-  surveyStarted,
-  surveyOutPeriod,
-  surveyNextResponse,
+	setSelectedSurvey,
+	surveyAnswered,
+	surveyStarted,
+	surveyOutPeriod,
+	surveyNextResponse,
 } from "~/actions/survey";
 import { getUserToken } from "~/api/utils";
 
@@ -22,237 +22,280 @@ const d = console.log;
 const j = (m) => JSON.stringify(m, null, 4);
 
 const SurveysList = ({
-  l,
-  lang,
-  surveys,
-  user,
-  school,
-  contextSurvey,
-  setShowModalHowItWorks,
+	l,
+	lang,
+	surveys,
+	user,
+	school,
+	contextSurvey,
+	setShowModalHowItWorks,
 }) => {
-  const gotToSurvey = (survey) => {
-    setSelectedSurvey(survey);
-    const { type } = survey;
-    window.location =
-      type == "personal" ? "survey-context" : "/responder-questionario";
-  };
+	const showPrintContextSurveyBtn = user._profile === "teacher";
 
-  const gotToPrintSurvey = (survey) => {
-    setSelectedSurvey(survey);
-    window.open("/imprimir-questionario", "_blank");
-  };
+	const gotToSurvey = (survey) => {
+		setSelectedSurvey(survey);
+		const { type } = survey;
+		window.location =
+			type == "personal" ? "survey-context" : "/responder-questionario";
+	};
 
-  const gotToPrintSurveyContext = () => {
-    const url = `/print/survey/${contextSurvey.id}`;
-    window.open(url, "_blank");
-  };
+	return surveys.map((survey, idx) => {
+		const {
+			id: { $oid: idSurvey },
+			type,
+			schedule,
+		} = survey;
 
-  const showPrintContextSurveyBtn = user._profile === "teacher";
+		if (type == "context") {
+			return null;
+		}
 
-  return surveys.map((survey, idx) => {
-    const {
-      id: { $oid: idSurvey },
-      type,
-      schedule,
-    } = survey;
+		const firstSchedule = schedule[0];
+		const hasDescr = !!firstSchedule.survey_description;
+		const showHowItWorks = type == "personal";
+		const answer = firstSchedule.answers.find(
+			(answer) =>
+				answer.status === "Complete" &&
+				answer.user_id.$oid === user._id.$oid
+		);
+		const hasAnswer = !!answer;
+		const surveyBtnLabel = hasAnswer
+			? "Resources.continueSurvey"
+			: "Resources.answerSurvey";
 
-    if (type == "context") {
-      return null;
-    }
-
-    const firstSchedule = schedule[0];
-    const hasDescr = !!firstSchedule.survey_description;
-    const showHowItWorks = type == "personal";
-    const answer = firstSchedule.answers.find(
-      (answer) =>
-        answer.status === "Complete" && answer.user_id.$oid === user._id.$oid
-    );
-    const hasAnswer = !!answer;
-
-    return (
-      <div className="container mb-30" key={idSurvey}>
-        <div className={classNames("columns is-multiline", styles.box_main)}>
-          <div className="column is-full">
-            <h1 className="is-size-3 has-text-weight-light mb-0">
-              {firstSchedule.survey_name}
-            </h1>
-            {hasDescr && (
-              <p className="mt-1">{firstSchedule.survey_description}</p>
-            )}
-          </div>
-          {showHowItWorks && (
-            <HowItWorks
-              label={l("LoginEducator.howWorks")}
-              setShowModalHowItWorks={setShowModalHowItWorks}
-            />
-          )}
-          <div className="column is-full">
-            {survey.is_cyclic && schedule.length > 0 && firstSchedule.name && (
-              <h3 className="is-size-6">
-                <span className="has-text-weight-bold">
-                  {parse(l("Resources.currentCycle"))}:
-                </span>{" "}
-                {firstSchedule.name}
-              </h3>
-            )}
-            {!surveyOutPeriod(survey) &&
-              !surveyAnswered(survey, user) &&
-              isDirectorOrTeacher(user) &&
-              !hasAnswer && (
-                // Boton de responder cuestionario
-                <Button
-                  className={classNames(
-                    "is-primary ml-0",
-                    styles.resources__buttons__button
-                  )}
-                  onClick={() => gotToSurvey(survey)}
-                >
-                  <span className={styles.with_icon}>
-                    <i
-                      className={classNames(
-                        "fas fa-clipboard-list is-size-5 mr-10",
-                        styles.fa
-                      )}
-                    ></i>
-                    {surveyStarted(survey, user)
-                      ? parse(l("Resources.continueSurvey"))
-                      : parse(l("Resources.answerSurvey"))}
-                  </span>
-                </Button>
-              )}
-            {surveyOutPeriod(survey) &&
-            !survey.is_cyclic &&
-            isDirectorOrTeacher(user) ? (
-              <div>
-                <p>{parse(l("Resources.description1"))} </p>
-              </div>
-            ) : (
-              <span>
-                {showPrintContextSurveyBtn && (
-                  <Button
-                    className={classNames(styles.resources__buttons__button)}
-                    onClick={() => gotToPrintSurveyContext()}
-                  >
-                    <span className={styles.with_icon}>
-                      <i className="fas fa-print is-size-5 mr-10"></i>
-                      {parse(l("Resources.printContext"))}
-                    </span>
-                  </Button>
-                )}
-                <Button
-                  className={classNames(styles.resources__buttons__button)}
-                  onClick={() => gotToPrintSurvey(survey)}
-                >
-                  <span className={styles.with_icon}>
-                    <i className="fas fa-print is-size-5 mr-10"></i>
-                    {parse(l("Resources.print"))}
-                  </span>
-                </Button>
-              </span>
-            )}
-          </div>
-          {hasAnswer && (
-            <HasAnswer
-              l={l}
-              lang={lang}
-              answer={answer}
-              schedule={schedule}
-              survey={survey}
-              user={user}
-              school={school}
-            />
-          )}
-        </div>
-      </div>
-    );
-  });
+		return (
+			<div className="container mb-30" key={idSurvey}>
+				<div className={classNames("", styles.box_main)}>
+					<div className="column is-full">
+						<h1 className="is-size-3 has-text-weight-light mb-0">
+							{firstSchedule.survey_name}
+						</h1>
+						{hasDescr && (
+							<p className="mt-1">
+								{firstSchedule.survey_description}
+							</p>
+						)}
+					</div>
+					<div className="column is-full">
+						{survey.is_cyclic &&
+							schedule.length > 0 &&
+							firstSchedule.name && (
+								<h3 className="is-size-6">
+									<span className="has-text-weight-bold">
+										{parse(l("Resources.currentCycle"))}:
+									</span>{" "}
+									{firstSchedule.name}
+								</h3>
+							)}
+						{/*{surveyOutPeriod(survey) &&
+						!survey.is_cyclic &&
+						isDirectorOrTeacher(user) ? (
+							<div>
+								<p>{parse(l("Resources.description1"))} </p>
+							</div>
+						) : (
+							AQUIIIIIII
+						)}*/}
+					</div>
+					{hasAnswer ? (
+						<HasAnswer
+							l={l}
+							lang={lang}
+							answer={answer}
+							schedule={schedule}
+							survey={survey}
+							user={user}
+							school={school}
+						/>
+					) : (
+						<Button
+							className={classNames(
+								"is-primary ml-0",
+								styles.resources__buttons__button
+							)}
+							onClick={() => gotToSurvey(survey)}
+						>
+							<span className={styles.with_icon}>
+								<i
+									className={classNames(
+										"fas fa-clipboard-list is-size-5 mr-10",
+										styles.fa
+									)}
+								></i>
+								{parse(l(surveyBtnLabel))}
+							</span>
+						</Button>
+					)}
+					<Actions
+						l={l}
+						survey={survey}
+						contextSurvey={contextSurvey}
+						setSelectedSurvey={setSelectedSurvey}
+						showPrintContextSurveyBtn={true}
+						showAnswerBtn={true}
+						showHowItWorks={showHowItWorks}
+						setShowModalHowItWorks={setShowModalHowItWorks}
+					/>
+				</div>
+			</div>
+		);
+	});
 };
 
 const HasAnswer = ({ l, schedule, lang, survey, answer, user, school }) => {
-  const openFeedback = () => {
-    window.open(
-      CONF.ApiURL +
-        "/api/v1/survey/feedback/" +
-        survey.id +
-        "/" +
-        answer.id.$oid +
-        "?access_token=" +
-        getUserToken() +
-        "&lang=" +
-        lang,
-      "target=_blank"
-    );
-  };
+	const openFeedback = () => {
+		window.open(
+			CONF.ApiURL +
+				"/api/v1/survey/feedback/" +
+				survey.id +
+				"/" +
+				answer.id.$oid +
+				"?access_token=" +
+				getUserToken() +
+				"&lang=" +
+				lang,
+			"target=_blank"
+		);
+	};
 
-  return (
-    <div>
-      <div className="column is-8 is-offset-2 mt-30 mb-20">
-        <div className="has-text-weight-bold is-size-6">
-          {parse(l("Resources.historicTitle"))}
-        </div>
-      </div>
-      <div className={classNames("column is-8 is-offset-2", styles.history)}>
-        <div className="columns">
-          {school && isDirector(user) && survey.type == "school" && (
-            <div className="column has-text-weight-bold">
-              {parse(l("Resources.cycle"))}
-            </div>
-          )}
-          <div className="column has-text-weight-bold">
-            {parse(l("Resources.answered"))}
-          </div>
-          <div className="column has-text-weight-bold">
-            {parse(l("Resources.devolutive"))}
-          </div>
-        </div>
-        {schedule.map(
-          (schedule) =>
-            schedule.answers &&
-            schedule.answers.map((answer, idxAns) =>
-              answer.status === "Complete" && answer.results !== null &&
-              answer.user_id.$oid === user._id.$oid &&
-              answer.type !== "Combined" ? (
-                <div className="columns" key={answer.id.$oid}>
-                  {school && isDirector(user) && survey.type == "school" && (
-                    <div className="column">{schedule.name}</div>
-                  )}
-                  <div className="column">
-                    {school &&
-                      isDirector(user) &&
-                      survey.type == "school" && [answer.user_name, " - "]}
-                    {moment(answer.submitted_at).format("DD/MM/YYYY")}
-                  </div>
-                  <div className="column">
-                    <a
-                      className={styles.access_link}
-                      onClick={() => openFeedback()}
-                    >
-                      <span className={styles.with_icon}>
-                        {parse(l("Resources.accessDevolutive"))}
-                        <i className="ml-10 far fa-file-pdf"></i>
-                      </span>
-                    </a>
-                  </div>
-                </div>
-              ) : null
-            )
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="column mt-30 mb-20">
+				<div className="has-text-weight-bold is-size-6">
+					{parse(l("Resources.historicTitle"))}
+				</div>
+			</div>
+			<div
+				className={classNames(
+					"column is-8 is-offset-2",
+					styles.history
+				)}
+			>
+				<div className="columns">
+					{school && isDirector(user) && survey.type == "school" && (
+						<div className="column has-text-weight-bold">
+							{parse(l("Resources.cycle"))}
+						</div>
+					)}
+					<div className="column has-text-weight-bold">
+						{parse(l("Resources.answered"))}
+					</div>
+					<div className="column has-text-weight-bold">
+						{parse(l("Resources.devolutive"))}
+					</div>
+				</div>
+				{schedule.map(
+					(schedule) =>
+						schedule.answers &&
+						schedule.answers.map((answer, idxAns) =>
+							answer.status === "Complete" &&
+							answer.results !== null &&
+							answer.user_id.$oid === user._id.$oid &&
+							answer.type !== "Combined" ? (
+								<div className="columns" key={answer.id.$oid}>
+									{school &&
+										isDirector(user) &&
+										survey.type == "school" && (
+											<div className="column">
+												{schedule.name}
+											</div>
+										)}
+									<div className="column">
+										{school &&
+											isDirector(user) &&
+											survey.type == "school" && [
+												answer.user_name,
+												" - ",
+											]}
+										{moment(answer.submitted_at).format(
+											"DD/MM/YYYY"
+										)}
+									</div>
+									<div className="column">
+										<a
+											className={styles.access_link}
+											onClick={() => openFeedback()}
+										>
+											<span className={styles.with_icon}>
+												{parse(
+													l(
+														"Resources.accessDevolutive"
+													)
+												)}
+												<i className="ml-10 far fa-file-pdf"></i>
+											</span>
+										</a>
+									</div>
+								</div>
+							) : null
+						)
+				)}
+			</div>
+		</div>
+	);
 };
 
-const HowItWorks = ({ label, setShowModalHowItWorks }) => {
-  return (
-    <div className="column is-full">
-      <Button
-        className={classNames("ml-0 mb-0", styles.resources__buttons__button)}
-        onClick={() => setShowModalHowItWorks(true)}
-      >
-        {label}
-      </Button>
-    </div>
-  );
+const Actions = ({
+	l,
+	survey,
+	contextSurvey,
+	setSelectedSurvey,
+	showPrintContextSurveyBtn,
+	showHowItWorks,
+	setShowModalHowItWorks,
+}) => {
+	const gotToPrintSurvey = (survey) => {
+		setSelectedSurvey(survey);
+		window.open("/imprimir-questionario", "_blank");
+	};
+
+	const gotToPrintSurveyContext = () => {
+		const url = `/print/survey/${contextSurvey.id}`;
+		window.open(url, "_blank");
+	};
+
+	return (
+		<div className="mt-4">
+			{/*Boton de Como funciona*/}
+			{showHowItWorks && (
+				<Button
+					className={classNames(
+						"ml-0 mb-0 is-small",
+						styles.resources__buttons__button
+					)}
+					onClick={() => setShowModalHowItWorks(true)}
+				>
+					{l("LoginEducator.howWorks")}
+				</Button>
+			)}
+			{showPrintContextSurveyBtn && (
+				<Button
+					className={classNames(
+						"is-small",
+						styles.resources__buttons__button
+					)}
+					onClick={() => gotToPrintSurveyContext()}
+				>
+					<span className={styles.with_icon}>
+						<i className="fas fa-print mr-10"></i>
+						{parse(l("Resources.printContext"))}
+					</span>
+				</Button>
+			)}
+			<Button
+				className={classNames(
+					"is-small",
+					styles.resources__buttons__button
+				)}
+				onClick={() => gotToPrintSurvey(survey)}
+			>
+				<span className={styles.with_icon}>
+					<i className="fas fa-print mr-10"></i>
+					{parse(l("Resources.print"))}
+				</span>
+			</Button>
+		</div>
+	);
 };
 
 export default SurveysList;
